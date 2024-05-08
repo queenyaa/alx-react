dde8f25171e8ee72bf00b8ec37d94eb1e4c83d0e
queenyaa <ysgyan@gmail.com>
2024-05-08 15:20:16 +0000

    advanced

---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,4 @@
+const $ = require( "jquery" );
+import "../../modules/footer/footer.css";
+
+$('body').append("<p>Copyright - Holberton School</p>");
---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    hints: false,
+    maxEntrypointSize: 1000000,
+  },
+  plugins: [ new HtmlWebpackPlugin(), new CleanWebpackPlugin() ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+  },
+  devServer: {
+    port: 8564,
+    open: true,
+    static: {
+      directory: path.resolve(__dirname, 'public')
+    }
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
