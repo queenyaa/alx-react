0ab99d4b6928b322529c0c8f5557ec9dc516c51f
queenyaa <ysgyan@gmail.com>
2024-05-08 15:39:31 +0000

    setup

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -18,7 +18,16 @@ module.exports = {
         },
         {
             test: /\.(png|jpe?g|gif|svg)$/i,
-            type: 'asset/resource',
+            use: [
+                "file-loader",
+		{
+                    loader: 'image-webpack-loader',
+                    options: {
+		      bypassOnDebug: true,
+		      disable: true,
+                    },
+                },
+            ],
         },
     ],
   },
