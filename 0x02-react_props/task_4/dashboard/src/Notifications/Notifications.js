2b09f7bc54fe473b5e0212c4aad2a879b2c4b88b
queenyaa <ysgyan@gmail.com>
2024-05-12 12:13:54 -0500

    task 4

---
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../Notifications/Notifications.css';
+import closeIcon from './../assets/closeicon.png';
+import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem';
+
+function Notifications() {
+    const handleClick = () => {
+        console.log('Close button has been clicked');
+    };
+
+    return (
+        <div className="Notifications">
+            <button
+                style={{
+                    position: 'absolute',
+                    border: 'none',
+                    top: 10,
+                    right: 10,
+                }}
+                aria-label="Close"
+                onClick={handleClick}
+                >
+                <img src={closeIcon} alt="Close icon" height="10" width="10"/>
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <NotificationItem type="default" value="New course available" />
+                <NotificationItem type="urgent" value="New resume available" />
+                <NotificationItem type="urgent" html={{ __html: getLatestNotification() }} />
+            </ul>
+        </div>
+    );
+}
+
+Notifications.defaultProps = {
+    displayDrawer: false,
+  };
+  
+  Notifications.propTypes = {
+    displayDrawer: PropTypes.bool,
+  };
+  
+  export default Notifications;
+
