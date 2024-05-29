f00c54d23eb9ca127952718f9047335b345dc28f
queenyaa <ysgyan@gmail.com>
2024-05-28 22:08:28 -0500

    Converting into an Immutable object using fromJS

--- 0x06-React_Immutable/0-main.js
+++ 0x06-React_Immutable/0-main.js
@@ -1,15 +1,15 @@
-const { getImmutableObject } = require('./0-fromjs');
-const { isMap } = require('immutable');
+import getImmutableObject from './0-fromjs';
+// const { isMap } = require('immutable');
 
-const obj = {
+const object = {
   fear: true,
   smell: -1033575916.9145899,
   wall: false,
   thing: -914767132
 };
 
-const immutableObj = getImmutableObject(obj);
+console.log(getImmutableObject(object))
 
-console.log(immutableObj);
-console.log(isMap(immutableObj)); 
+// console.log(immutableObj);
+// console.log(isMap(immutableObj)); 
 // Should print true if the object is converted correctly
