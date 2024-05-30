23715c294b0209d4b9fc84e091ec7db048c304af
queenyaa <ysgyan@gmail.com>
2024-05-29 21:20:01 -0500

    Nested merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,9 +1,8 @@
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 
 // Function to deeply merge two objects into an Immutable List
 export default function mergeDeeplyElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
-  const mergedMap = map1.mergeDeep(map2);
-  return List(mergedMap.values());
+  return map1.mergeDeep(map2);
 }
