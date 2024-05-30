d3c7ad803625c9bdeb0f437de5c7819438adc767
queenyaa <ysgyan@gmail.com>
2024-05-29 20:12:40 -0500

    Chained mutations

---
+++ 0x06-React_Immutable/4-main.js
@@ -0,0 +1,9 @@
+import { map, map2 } from './4-mutations.js';
+
+// Log the original map
+console.log("Original map:");
+console.log(map.toString());
+
+// Log the modified map
+console.log("Modified map:");
+console.log(map2.toString());
---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,17 @@
+import { Map } from 'immutable';
+
+// Create an Immutable Map with the given object
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+// Use map to derive map2 with specified modifications
+export const map2 = map.withMutations(m => {
+  m.set(2, 'Benjamin');
+  m.set(4, 'Oliver');
+});
